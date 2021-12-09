7c56cd7c3f37588449dae250d294e5831efac65f
Keiya Honda <e195721@ie.u-ryukyu.ac.jp>
2021-12-09 10:14:51 +0900

    ブランチ作成とコードのクラス化

--- test.py
+++ test.py
@@ -1,26 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
 import sys
 import tkinter
+from tkinter.constants import LEFT
+from tkinter import ttk
+class Application(tkinter.Frame):
+  def _init_(self,master):
+    # super().__init__(master)
+    # self.pack()
+    
+    self.master.title(u"Software Title")###ここから
+    self.master.geometry("400x300")
+    self.EDITDEFAULT_X=160
+    self.EDITDEFAULT_Y=20
+    self.DEFAULTENTRYBOX_X=130 #最初の入力ボックスのx座標
+    self.DEFAULTENTRYBOX_Y=40 #最初の入力ボックスのy座標
+    self.DEFAULTCHECKBOX_X=0
+    self.DEFAULTCHECKBOX_Y=40
+    
+    self.box_y=0
+    self.check_box_y=0
+    
+    self.create_widgets()
 
-root = tkinter.Tk()
-root.title(u"もっと...熱くなれよおおおおおおおおお")
-root.geometry("400x300")
-
-def DeleteEntryValue(event):
-    #エントリーの中身を削除
-    EditBox.delete(0, tkinter.END)
-
-EditBox = tkinter.Entry(width=50)
-EditBox.insert(tkinter.END,"てすと")
-EditBox.pack()
-
-EditBox.delete(0, tkinter.END)
-
-value = EditBox.get()
-
-Button = tkinter.Button(text=u'ボタンです')
-Button.bind("<Button-1>",DeleteEntryValue) 
-Button.pack()
+  # Create Widgets function
+  def create_widgets(self):
+      pass
 
 
+def main():
+  root = tkinter.Tk()
+  app = Application(master = root)
+  app.mainloop()###ここまで実行内容
 
-root.mainloop()
+if __name__ == "__main__":
+  main()
